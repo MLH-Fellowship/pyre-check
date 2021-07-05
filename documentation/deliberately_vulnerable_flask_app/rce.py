0e888e831c8f39d556d5222edfe4c28ceb814f16
Pulkit Mishra <pulkitmishra007@gmail.com>
2021-07-05 18:43:45 +0530

    adds vulnerabilities in flask app

--- documentation/deliberately_vulnerable_flask_app/rce.py
+++ documentation/deliberately_vulnerable_flask_app/rce.py
@@ -4,9 +4,10 @@
 # LICENSE file in the root directory of this source tree.
 
 import subprocess
-
+from MySQLdb import _mysql
 from flask import Flask
-
+import request
+from lxml import etree
 
 app = Flask(__name__)
 
@@ -24,3 +25,32 @@ def potential_rce_1(payload: str) -> None:
 @app.route("/rce/<int:payload>")
 def potential_rce_2(payload: int) -> None:
     subprocess.run(f"echo {payload}", shell=True)
+
+
+@app.route("/pt/<string:payload>")
+def definite_pt(payload: str) -> str:
+    f = open(payload, "r")
+    text = f.read()
+    return text
+
+
+@app.route("/xss/<string:payload>")
+def definite_xss(payload: str) -> None:
+    flask.Markup(payload)
+
+
+@app.route("/sql/<string:payload>")
+def definite_sql(payload: str) -> None:
+    con = _mysql.connect()
+    cur = con.cursor()
+    cur.execute(payload)
+
+
+@app.route("/ssrf/<string:payload>")
+def definite_ssrf(payload: str) -> None:
+    requests.get(payload)
+
+
+@app.route("/xxe/<string:payload>")
+def definite_xxe(payload: str) -> None:
+    doc = etree.fromstring(payload)
